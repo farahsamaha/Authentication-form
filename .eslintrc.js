2df9d4e584d185dae88edf99238337c6312a0a22
farahsamaha <farahsamaha121@gmail.com>
2022-12-04 12:50:40 +0200

    int pro

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,15 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/standard'
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': ['error', 'always-multiline']
   }
 }
